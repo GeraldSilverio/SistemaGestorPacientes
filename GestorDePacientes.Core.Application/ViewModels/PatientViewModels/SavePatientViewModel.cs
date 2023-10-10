846ae1e56f3acaa100eda7897d5f145bb488d933
Gerald Antonio Silverio Serrata <20221068@itla.edu.do>
2023-10-09 23:08:06 -0400

    Update method completed

--- GestorDePacientes.Core.Application/ViewModels/PatientViewModels/SavePatientViewModel.cs
+++ GestorDePacientes.Core.Application/ViewModels/PatientViewModels/SavePatientViewModel.cs
@@ -11,17 +11,25 @@ namespace GestorDePacientes.Core.Application.ViewModels.PatientViewModels
     public class SavePatientViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Debe completar este campo")]
         public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "Debe completar este campo")]
         public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "Debe completar este campo")]
         public string PhoneNumber { get; set; } = null!;
+        [Required(ErrorMessage = "Debe completar este campo")]
         public string Identification { get; set; } = null!;
-        public DateTime DateOfBorn { get; set; } =DateTime.Now;
+        [Required(ErrorMessage = "Debe completar este campo")]
+        public DateTime DateOfBorn { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Debe completar este campo")]
         public string Direction { get; set; } = null!;
 
         [DataType(DataType.Upload)]
-        public IFormFile File { get; set; } = null!;
-        public string ImageUrl { get; set; }
+        public IFormFile? File { get; set; }
+        public string? ImageUrl { get; set; }
+        [Required(ErrorMessage = "Debe completar este campo")]
         public bool IsSmoker { get; set; }
+        [Required(ErrorMessage = "Debe completar este campo")]
         public bool IsAllegier { get; set; }
     }
 }
--- SistemaGestorPacientes/Controllers/PatientController.cs
+++ SistemaGestorPacientes/Controllers/PatientController.cs
@@ -27,6 +27,10 @@ namespace WebApp.SistemaGestorPacientes.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return View("Create", vm);
+                }
                 SavePatientViewModel patientCreated = await _patientService.Add(vm);
 
                 if(patientCreated != null && patientCreated.Id !=0)
@@ -53,7 +57,24 @@ namespace WebApp.SistemaGestorPacientes.Controllers
         [HttpPost]
         public async Task<IActionResult> Update(SavePatientViewModel vm)
         {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("Create", vm);
+                }
+                SavePatientViewModel patientCreated = await _patientService.GetById(vm.Id);
 
+                if (patientCreated != null && patientCreated.Id != 0)
+                {
+                    patientCreated.ImageUrl = _patientService.UplpadFile(vm.File, patientCreated.Id,true,patientCreated.ImageUrl);
+                    await _patientService.Update(vm, vm.Id);
+                }
+                return RedirectToRoute(new { controller = "Patient", action = "Index" });
+            }catch(Exception ex)
+            {
+                return View("Index", ex);
+            }
         }
 
 
